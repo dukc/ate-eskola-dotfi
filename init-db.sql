911c91c559dfc9d69fd216fcf670f8da91d9006e
Ate Eskola <ajieskola@gmail.com>
2025-05-14 12:33:06 +0300

    Korjattu komento init-db:ssä

--- init-db.sql
+++ init-db.sql
@@ -4,4 +4,4 @@ create table visitors
     private_message varchar(16384),
     time timestamp not null
 );
-create index visitors_by_date on visitors asc time;
+create index visitors_by_date on visitors (time asc);
